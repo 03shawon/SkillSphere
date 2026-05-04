0555823b0ac2ba61a153f4356a304bf5efc03ea3
03shawon <shawoncse5089@gmail.com>
2026-05-04 14:25:15 +0600

    signin and signup authentication complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 "use client";
 
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button } from "@heroui/react";
 import Link from "next/link";
 import { useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = () => {
+    const userData = authClient.useSession();
+    const user = userData.data?.user
+    // console.log(user)
+    const handleSignOut = async () => {
+        await authClient.signOut();
+    }
     const [open, setOpen] = useState(false);
 
     return (
@@ -33,24 +41,33 @@ const Navbar = () => {
                     <li><Link href="/profile">My Profile</Link></li>
                 </ul>
 
+                {open && (
+                    <div className="md:hidden mt-3 pb-3 border-t">
+                        <ul className="flex flex-col gap-4 pt-3 text-sm font-medium">
+                            <li><Link href="/">Home</Link></li>
+                            <li><Link href="/all-courses">All Courses</Link></li>
+                            <li><Link href="/profile">My Profile</Link></li>
+                        </ul>
+                    </div>
+                )}
                 <div className="flex items-center gap-4">
-                    <ul className="flex items-center text-sm gap-5">
-                        <li><Link href="/signup">SignUp</Link></li>
-                        <li><Link href="/signin">SignIn</Link></li>
-                        <li><Link href="/signout">SignOut</Link></li>
-                    </ul>
+                    {!user && (<ul className="flex items-center text-sm gap-3 ">
+                        <li><Link href="/signin" className="border rounded-2xl p-3 bg-blue-200">SignIn</Link></li>
+                        <li><Link href="/signup" className="border rounded-2xl p-3 bg-blue-200">SignUp</Link></li>
+                    </ul>)}
+                    {user && (
+                        <div className="flex gap-3">
+                            <Avatar size="sm">
+                                <Avatar.Image alt="John Doe" src={user?.image} referrerPolicy="no-referrer"
+                                />
+                                <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+                            </Avatar>
+                            <Button variant="danger-soft" onClick={handleSignOut} size="sm">SignOut</Button>
+                        </div>
+                    )}
                 </div>
             </nav>
 
-            {open && (
-                <div className="md:hidden mt-3 pb-3 border-t">
-                    <ul className="flex flex-col gap-4 pt-3 text-sm font-medium">
-                        <li><Link href="/">Home</Link></li>
-                        <li><Link href="/all-courses">All Courses</Link></li>
-                        <li><Link href="/profile">My Profile</Link></li>
-                    </ul>
-                </div>
-            )}
         </div>
     );
 };
