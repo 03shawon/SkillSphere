2f6efaefa884e9a2c5c7c2a10329e36cf94561bc
03shawon <shawoncse5089@gmail.com>
2026-05-04 18:19:37 +0600

    vercel redeploy

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://skillsphere-psi-two.vercel.app"
+    // baseURL: "https://skillsphere-psi-two.vercel.app"
+    baseURL: process.env.NEXT_PUBLIC_APP_URL || window.location.origin
 })
+
